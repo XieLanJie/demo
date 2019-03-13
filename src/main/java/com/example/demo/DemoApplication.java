b20fa5f61ea3e772321396c38c1ebff60e60d4ea
7800.7800xie <1393741411@qq.com>
2019-03-13 11:56:24 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("demo")
+@SpringBootApplication
+public class DemoApplication {
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
